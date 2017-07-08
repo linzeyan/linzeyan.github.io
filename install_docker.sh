46285fdaa4f84d9af68c93b3458b51e6ff34d3c1
ZeYanLin <linzeyan@users.noreply.github.com>
2017-07-08 22:57:50 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,12 +3,25 @@
 sudo apt-get update \
   && sudo apt-get upgrade \
   && sudo apt-get dist-upgrade \
-  && sudo apt-get install ssh \
-                          bridge-utils \
-                          uml-utilities \
+  && sudo apt-get install  -y --no-install-recommends \
+  ssh \
+  bridge-utils \
+  uml-utilities \
+  file \
+  git \
+  libapparmor1 \
+  libcurl4-openssl-dev \
+  libedit2 \
+  libssl-dev \
+  lsb-release \
+  psmisc \
+  python-setuptools \
+  sudo \
+  wget \  
   && curl -sSL https://get.docker.com/ | sudo sh \
   && sudo apt-get update \
   && sudo apt-get upgrade \
+  && rm -rf /tmp/*
 
 sudo service docker start
 sudo docker pull zeyanlin/rstudio:latest
