234a54c4847542dae549e05ae2664e43cda80eca
ZeYanLin <linzeyan@users.noreply.github.com>
2017-07-20 22:41:54 +0800

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # centos 6.8
+#------------------------------------------------------------
+# install nginx
+#------------------------------------------------------------
 sudo yum install -y --no-install-recommends \
   epel-release  \
   nginx
-  
+#------------------------------------------------------------
+# service start and open 80 port
+#------------------------------------------------------------
 sudo /etc/init.d/nginx start
 iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 /sbin/iptables -I INPUT -p tcp --dport 80 -j ACCEPT
