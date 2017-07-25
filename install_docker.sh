e4c655ed23d98a585d3aefc20488a9728fb71f63
ZeYanLin <linzeyan@users.noreply.github.com>
2017-07-25 21:43:56 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -6,7 +6,7 @@
 sudo apt-get update \
   && sudo apt-get upgrade \
   && sudo apt-get dist-upgrade \
-  && sudo apt-get install  -y --no-install-recommends \
+  && sudo apt-get install  -y \
   ssh \
   bridge-utils \
   uml-utilities \
