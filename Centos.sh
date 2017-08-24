52f7e12189f1bcca99367f8291ddfb4cff5a3a29
ZeYanLin <linzeyan@users.noreply.github.com>
2017-08-24 21:42:33 +0800

    Update and rename centos.sh to Centos.sh

--- Centos.sh
+++ Centos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install -y mlocate.x86_64 epel-release nginx telnet nmap bind-utils gdisk* zip* ntpdate wget gcc jwhois.x86_64
+yum install -y mlocate.x86_64 epel-release nginx telnet nmap bind-utils gdisk* zip* ntpdate wget gcc jwhois
 yum groupinstall -y "Development tools"
 yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel
 yum install -y readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel epel-release
