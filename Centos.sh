aaa3fa2d3008688b2dbf1132e2436a8fdf2b9661
ZeYanLin <linzeyan@users.noreply.github.com>
2018-02-03 10:07:10 +0800

    Update Centos.sh

--- Centos.sh
+++ Centos.sh
@@ -3,10 +3,7 @@ yum install -y mlocate.x86_64 epel-release nginx telnet nmap bind-utils gdisk* z
 yum groupinstall -y "Development tools"
 yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel
 yum install -y readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel epel-release
-#- Install Tcpping
-#1 rpm -ivh ftp://195.220.108.108/linux/dag/redhat/el7/en/x86_64/dag/RPMS/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
-#2 yum -y install tcptraceroute
-#3 cd /usr/bin
-#4 wget http://www.vdberg.org/~richard/tcpping
-#5 chmod 755 tcpping
+yum install -y tcptraceroute && wget -O /usr/bin/tcpping http://www.vdberg.org/~richard/tcpping && chmod 755 /usr/bin/tcpping
+yum  install -y lrzsz gcc-c++  make man vim unzip wget curl lua-devel lua-static patch libxml2-devel libxslt libxslt-devel gd gd-devel ntp ntpdate screen sysstat tree rsync lsof openssh-clients telnet htop libselinux-python
+
 
